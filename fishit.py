f6230344a0c24ab10c42b1ceacd0119d9cc7f01a
Andres Morales Reyes <sharker92@hotmail.com>
2020-09-29 18:14:33 -0600

    Fixing start and finishing spider, missing test

--- fishit.py
+++ fishit.py
@@ -12,12 +12,18 @@ kval_pairs = {"agrupacion": "1", "departamento": "1",
 page = requests.get("https://alsuper.com/categorias", params=kval_pairs)
 print("Ingresando a: ", page.url)  # print the url that was fetched
 print("Status: ", page.status_code, page.history)
-print(type(page))
+# print("Content-Type: ", page.headers["content-type"])
+# print("Content-Type: ", page.headers["content-type"][:9])
+print(type(page.text))
 print(page.headers)
 soup = BeautifulSoup(page.text, "html.parser")
 print(type(soup))
 
-list = soup.find_all("ul", class_="row list-unstyled products--list")
+tag = soup("a")
+tag2 = soup.find_all("a")
+print(tag == tag2)
+
+# list = soup.find_all("ul", class_="row list-unstyled products--list")
 # print(list)
 
 with open("alsuper.txt", "w") as f:
--- fishit_spider.py
+++ fishit_spider.py
@@ -1,4 +1,8 @@
 import sqlite3
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+from urllib.parse import urljoin
 
 
 def deleteTables():
@@ -15,6 +19,7 @@ def createTables():
         url TEXT UNIQUE,
         html TEXT DEFAULT NULL,
         date TEXT DEFAULT NULL,
+        interest INTEGER DEFAULT NULL,
         error INTEGER DEFAULT NULL
         );
 
@@ -29,7 +34,7 @@ cur = conn.cursor()
 createTables()
 # https://alsuper.com/
 
-
+# Input new pages
 while True:
     start_url = input('Enter a new web url or enter to crawl: ')
     if start_url == "reset":
@@ -37,7 +42,10 @@ while True:
         print("Tables deleted")
         createTables()
         print("Tables created")
-    elif start_url:
+    if start_url != '' and not start_url.startswith('http'):
+        print('Please enter a valid url or click enter to continue. ')
+        continue
+    if start_url:
         if (start_url.endswith('/')):
             start_url = start_url[:-1]
         web = start_url
@@ -50,7 +58,7 @@ while True:
             conn.commit()
     else:
         cur.execute(
-            'SELECT id,url FROM Pages WHERE html is NULL and error is NULL ORDER BY RANDOM() LIMIT 1')  # revisar si es así o mejor en webs
+            'SELECT id,url FROM Pages WHERE html is NULL and interest is NULL or 1 and error is NULL ORDER BY RANDOM() LIMIT 1')  # revisar si es así o mejor en webs
         row = cur.fetchone()
         if row is None:
             print("No Webs founded to crawl.\nPlease, try Again.")
@@ -58,6 +66,8 @@ while True:
             break
         print(type(row))
         print(row)
+
+# Crawl on current pages
 print("Starting crawl on:")
 cur.execute('''SELECT url FROM Webs''')
 webs = list()
@@ -72,3 +82,116 @@ while True:
             break
         many_pgs = int(sval)
     many_pgs -= 1
+    cur.execute(
+        'SELECT id,url FROM Pages WHERE html is NULL and interest is NULL or 1 and error is NULL ORDER BY RANDOM() LIMIT 1')  # Date < Date Now
+    try:
+        row = cur.fetchone()
+        # print("Fetching: ", row)
+        fromid = row[0]
+        url = row[1]
+    except:
+        print('No unretrieved HTML pages found')
+        many_pgs = 0
+        break
+    print("Fetching:")
+    print(fromid, url, end=' ')
+
+    try:
+        document = requests.get(url)
+
+        html = document.text
+
+        if document.status_code != 200:
+            print("Error on page: ", document.status_code)
+            cur.execute('UPDATE Pages SET error=? WHERE url=?',
+                        (document.status_code, url))
+
+        if 'text/html' != document.headers["content-type"][:9]:
+            print("Ignore non text/html page")
+            cur.execute('UPDATE Pages SET interest=?', (0, ))
+            # cur.execute('DELETE FROM Pages WHERE url=?', (url, )) CODIGO ORIGINAL
+            conn.commit()
+            continue
+
+        print('('+str(len(html))+')', end=' ')
+
+        soup = BeautifulSoup(html, "html.parser")
+    except KeyboardInterrupt:
+        print('')
+        print('Program interrupted by user...')
+        break
+    except:
+        print("Unable to retrieve or parse page")
+        cur.execute('UPDATE Pages SET error=-1 WHERE url=?', (url, ))
+        conn.commit()
+        continue
+
+    # cur.execute('INSERT OR IGNORE INTO Pages (url, html) VALUES ( ?, NULL)', (url, )) #necesario¿?
+    cur.execute('UPDATE Pages SET html=? WHERE url=?',
+                (memoryview(bytes(html, encoding='utf-8')), url))
+    conn.commit()
+
+    # Retrieve all of the anchor tags
+    tags = soup.find_all('a')
+    count = 0
+    for tag in tags:
+        href = tag.get('href', None)
+        print()
+        print(tag)
+        print("href: ", href)
+        input()
+
+        if (href is None):
+            continue
+        # Resolve relative references like href="/contact"
+        up = urlparse(href)
+        if (len(up.scheme) < 1):
+            href = urljoin(url, href)
+        ipos = href.find('#')
+        if (ipos > 1):
+            href = href[:ipos]
+
+        print("#href: ", href)
+        input()
+
+        if (href.endswith('.png') or href.endswith('.jpg') or href.endswith('.gif')):
+            continue
+        if (href.endswith('/')):
+            href = href[:-1]
+        # print href
+        if (len(href) < 1):
+            continue
+
+        # Check if the URL is in any of the webs
+        found = False
+        for web in webs:
+            if (href.startswith(web)):
+                found = True
+                break
+        if not found:
+            continue
+
+        cur.execute(
+            'INSERT OR IGNORE INTO Pages (url, html ) VALUES ( ?, NULL )', (href, ))
+        count = count + 1
+        conn.commit()
+
+        cur.execute('SELECT id FROM Pages WHERE url=? LIMIT 1', (href, ))
+        try:
+            row = cur.fetchone()
+            toid = row[0]
+        except:
+            print('Could not retrieve id')
+            continue
+        # print fromid, toid
+        # cur.execute('INSERT OR IGNORE INTO Links (from_id, to_id) VALUES ( ?, ? )', (fromid, toid))
+
+    print(count)
+
+cur.close()
+
+# todo save date when html is fetched
+# <a class = "newCity" href = "switchlocation?id_location=6" > Cd. Chihuahua < /a >
+# href:  switchlocation?id_location = 6
+# <a class="newCity" href="switchlocation?id_location=85">Saltillo</a>
+# href:  switchlocation?id_location=85
