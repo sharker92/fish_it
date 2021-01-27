f7f5a760edd745b80f877360180a7ea00759716b
Andres Morales Reyes <sharker92@hotmail.com>
2021-01-27 13:20:07 -0700

    Crawler Finished

--- fishit_spider.py
+++ fishit_spider.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from urllib.parse import urljoin
-from datetime import date
+from datetime import date, timedelta
 import time
 
 
@@ -71,7 +71,7 @@ while True:
         cur.execute(
             'INSERT OR IGNORE INTO Pages (url) VALUES ( ? )', (start_url, ))
         cur.execute(
-            'UPDATE Pages SET web_id = Webs.id From Webs WHERE Pages.url = Webs.url')
+            'UPDATE Pages SET web_id = (SELECT id From Webs WHERE Webs.url = ?) WHERE url = ?', (start_url, start_url))
 
         conn.commit()
         print("\nActual Webs: ")
@@ -85,87 +85,83 @@ while True:
         print("Found No Webs to crawl. Please, try Again.\n")
     else:
         break
-# ME QUEDE EN REVISAR TODO EL CRAWL.
-# TO DO Falta agregar web_id a Pages y referenciarlo con la tabla de webs
+
+
 # Crawler
 print("\nStarting crawl on:")
+# Get the current webs
 cur.execute('''SELECT url FROM Webs''')
-print(cur.fetchall(), "\n")
+webs = list()
+for row in cur:
+    webs.append(str(row[0]))
+print(webs, "\n")
 
-many_pgs = 0
 pgs_cnt = 0
+today = date.today()
+print("Today: ", today)
+cur.execute('INSERT OR IGNORE INTO Dates (date) VALUES ( ? )', (today,))
+conn.commit()
+start = time.time()
 while True:
 
-    today = date.today()
-    print(today)
-    cur.execute('INSERT OR IGNORE INTO Dates (date) VALUES ( ? )', (today,))
-    conn.commit()
-    # cur.execute('SELECT id,date From Dates WHERE date == ?', (today,))
-    # row = cur.fetchone()
-    # print(row)
-    # today_id = row[0]
-    #current_date = datetime.strptime(row[1], "%Y-%m-%d").date()
-    cur.execute('SELECT Pages.id, Pages.url FROM Pages WHERE (date_id is NULL or (SELECT date FROM Dates WHERE Dates.id = date_id < ?)) and (interest is NULL or 1) and error is NULL ORDER BY RANDOM() LIMIT 1', (today,))
-###
+    if today != date.today():
+        today = date.today()
+        print(today)
+        cur.execute('INSERT OR IGNORE INTO Dates (date) VALUES ( ? )', (today,))
+        conn.commit()
+
+    cur.execute(
+        'SELECT Pages.id, Pages.url FROM Pages WHERE (date_id is NULL or (SELECT date FROM Dates WHERE Dates.id = date_id < ?)) and (interest is NULL or 1) and error is NULL ORDER BY RANDOM() LIMIT 1', (today,))
     try:
         row = cur.fetchone()
-        # print("Fetching: ", row)
         fromid = row[0]
         url = row[1]
     except:
         print('No unretrieved HTML pages found')
-        # many_pgs = 0 creo ya no lo necesito
         break
     print("Fetching:")
     print(fromid, url, end=' ')
     try:
         document = requests.get(url)
+        pg_inf = {}
+        pg_inf["status_code"] = document.status_code
 
-        status_code = document.status_code
-
-        if status_code != 200:
-            print("Error on page: ", document.status_code)
+        if pg_inf["status_code"] != 200:
+            print("\nError on page: ", document.status_code)
             cur.execute('UPDATE Pages SET error=? WHERE url=?',
-                        (status_code, url))
+                        (pg_inf["status_code"], url))
 
-        content_type = document.headers["content-type"][:9]
-        # print(document.headers["content-type"])
-        if content_type != 'text/html':
+        pg_inf["content_type"] = document.headers["content-type"][:9]
+
+        if pg_inf["content_type"] != 'text/html':
             print("Ignore non text/html page")
             cur.execute('UPDATE Pages SET interest=? WHERE url=?', (0, url))
             conn.commit()
             continue
 
-        html = document.text
-        print('('+str(len(html))+')', end=' ')  # imprime longitud del html
+        pg_inf["html"] = document.text
+        # imprime longitud del html
+        print('('+str(len(pg_inf["html"]))+')', end=' ')
 
-        soup = BeautifulSoup(html, "html.parser")
+        soup = BeautifulSoup(pg_inf["html"], "html.parser")
     except KeyboardInterrupt:
-        print('')
-        print('Program interrupted by user...')
+        print('\n\nProgram interrupted by user...')
         break
     except:
         print("Unable to retrieve or parse page")
         cur.execute('UPDATE Pages SET error=-1 WHERE url=?', (url, ))
         conn.commit()
         continue
-    # cur.execute('INSERT OR IGNORE INTO Pages (url, html) VALUES ( ?, NULL)', (url, )) #necesario¿? #AQUI es posible que sí
-    #   cur.execute('INSERT OR IGNORE INTO Pages (url, html, new_rank) VALUES ( ?, NULL, 1.0 )', ( url, ) )
-    #   cur.execute('UPDATE Pages SET html=? WHERE url=?', (memoryview(html), url ) )
     cur.execute('UPDATE Pages SET html=?, date_id = (SELECT id FROM Dates WHERE date = ?) WHERE url=?',
-                (memoryview(bytes(html, encoding='utf-8')), today, url))
+                (memoryview(bytes(pg_inf["html"], encoding='utf-8')), today, url))
     conn.commit()
 
-    input("pause")
     # Retrieve all of the anchor tags
     tags = soup.find_all('a')
+
     count = 0
     for tag in tags:
         href = tag.get('href', None)
-        # print()
-        # print(tag)
-        # print("href: ", href)
-        # input()
 
         if (href is None):
             continue
@@ -176,10 +172,6 @@ while True:
         ipos = href.find('#')
         if (ipos > 1):
             href = href[:ipos]
-
-        # print("#href: ", href)
-        # input()
-
         if (href.endswith('.png') or href.endswith('.jpg') or href.endswith('.gif')):
             continue
         if (href.endswith('/')):
@@ -187,10 +179,12 @@ while True:
         if (len(href) < 1):
             continue
 
-        # Check if the URL is in any of the webs
+        # Check if the URL is in any of the webs (web delimiter)
         found = False
         for web in webs:
             if (href.startswith(web)):
+                cur.execute(
+                    'UPDATE Pages SET web_id = (SELECT id From Webs WHERE Webs.url = ?) WHERE url = ?', (web, href))
                 found = True
                 break
         if not found:
@@ -201,33 +195,15 @@ while True:
         count = count + 1
         conn.commit()
 
-        cur.execute('SELECT id FROM Pages WHERE url=? LIMIT 1', (href, ))
-        try:
-            row = cur.fetchone()
-            toid = row[0]
-        except:
-            print('Could not retrieve id')
-            continue
-        # print fromid, toid
-        # cur.execute('INSERT OR IGNORE INTO Links (from_id, to_id) VALUES ( ?, ? )', (fromid, toid))
-
     if count % 50 == 0:
         conn.commit()
     if count % 100 == 0:
         time.sleep(1)
     print(count)
+    pgs_cnt += 1
 
 cur.close()
-
-
-# que pasa cuando ya este trepado y necesite retrepar porque es otro día
-# TODO Revisar los de las fechas, no necesita ser NULL el html
-#cur.execute('SELECT id,url FROM Pages WHERE html is NULL and error is NULL and (interest is NULL or interest is  1) ORDER BY RANDOM() LIMIT 1')
-
-
-# location
-# me esta metiendo el interest como 0 por defecto
-
-# tipos de contenido
-# application/pdf
-#text/html; charset=utf-8
+end = time.time()
+total_time = timedelta(seconds=int(end - start))
+final_msg = "Retrieved {} pages on {} seconds".format(pgs_cnt, total_time)
+print(final_msg)
