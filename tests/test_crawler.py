e1240dbd40e8e63f7d3346e0fd155e5846d525e6
andres_morales_reyes <sharker92@hotmail.com>
2021-03-24 18:31:05 -0700

    finished implementation by classes, continue with tests

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -9,11 +9,12 @@ from src.databases.base import session_factory, delete_db
 # export PYTHONPATH="${PYTHONPATH}:~/OneDrive/03Programming/01_fish_it"
 
 
-@pytest.fixture
-def create_session():
+@pytest.fixture(scope="class")
+def create_session(request):
     print("CREATE")
     session = session_factory()
-    yield session
+    request.cls.session = session
+    yield
     session.close()
     delete_db()
     print("DELETE")
@@ -28,45 +29,46 @@ def fill_database(request):
     request.param
     print("TWO")
     print(request.param)
-    session.add(Page(url="url", interest=0, error=0))
+    request.cls.session.add(Page(url="url", interest=0, error=0))
     # session.add(Web(url=url))
 
 
-@pytest.mark.parametrize('url',
-                         [
-                             ('https://alsuper.com/'),
-                             ('https://www.walmart.com.mx'),
-                             ('https://www.liverpool.com.mx/'),
-                             ('not_an_url'),
-                             ('not an url.com'),
-                         ]
-                         )
-def test_add_url_to_db(url, create_session):
-    session = create_session
-    add_url_to_db(url)
-    if (url.endswith('/')):
-        url = url[:-1]
-    try:
-        web = None
-        web = session.query(Web).filter_by(url=url).one()
-    except NoResultFound:
-        assert(not web)
-    else:
-        assert(url == web.url)
+@pytest.mark.usefixtures("create_session")
+class Test_Basic_DB:
+    @pytest.mark.parametrize('url',
+                             [
+                                 ('https://alsuper.com/'),
+                                 ('https://www.walmart.com.mx'),
+                                 ('https://www.liverpool.com.mx/'),
+                                 ('not_an_url'),
+                                 ('not an url.com'),
+                             ]
+                             )
+    def test_add_url_to_db(self, url):
+        # session = create_session
+        add_url_to_db(url)
+        if (url.endswith('/')):
+            url = url[:-1]
+        try:
+            web = None
+            web = self.session.query(Web).filter_by(url=url).one()
+        except NoResultFound:
+            assert(not web)
+        else:
+            assert(url == web.url)
+
+    def test_input_today_at_db(self):
+        today = date.today()
+        return_today = input_today_at_db()
+        today_db = self.session.query(Date).filter_by(dated=today).one()
+        assert(today == today_db.dated and today == return_today)
 
 
-def test_input_today_at_db(create_session):
-    session = create_session
-    today = date.today()
-    return_today = input_today_at_db()
-    today_db = session.query(Date).filter_by(dated=today).one()
-    assert(today == today_db.dated and today == return_today)
-
-
-def test_get_new_url(create_session, fill_database):
-    print("three")
-    session = create_session
-    session.add(Date(dated='2020-05-11'))
-    today = input_today_at_db()
-    get_new_url(today)
-    assert(False)
+@pytest.mark.usefixtures("create_session", "fill_database")
+class Test_Pre_Loaded_DB:
+    def test_get_new_url(self):
+        print("three")
+        self.session.add(Date(dated='2020-05-11'))
+        today = input_today_at_db()
+        get_new_url(today)
+        assert(False)
